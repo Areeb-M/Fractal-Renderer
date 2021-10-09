3c50ac283e3b1dfba5aab104cf21e04b9ad5a4d1
Areeb <am0247@uah.edu>
2021-10-09 15:24:47 -0500

    Added rudimentary command line arguments

--- Program.go
+++ Program.go
@@ -1,38 +1,47 @@
 package main
 
 import (
+	"flag"
 	"image/png"
 	"os"
 )
 
 func main() {
-	params := RasterParameters{
-		MandelbrotSet,
-		1600,
-		900,
-		-.9,
-		.9,
-		-2.0,
-		1.2,
-		1024,
-		2.0,
+	fractalFunctionArgument := flag.String("function", "mandelbrot", "Name of the fractal equation to render")
+	renderXArgument := flag.Float64("rx", -0.75, "X coordinate for the center of the render")
+	renderIArgument := flag.Float64("ri", 0, "I coordinate for the center of the render")
+	renderWidthArgument := flag.Int("width", 1600, "The width of the render in pixels")
+	renderHeightArgument := flag.Int("height", 900, "The height of the render in pixels")
+	renderZoomArgument := flag.Float64("zoom", 1.0, "The zoom level of the render")
+	renderOutputArgument := flag.String("output", "output.png", "The filename for the output")
+	fractalIterationsArgument := flag.Int("iterations", 1024, "The maximum number of iterations for each point being rendered")
+
+	flag.Parse()
+
+	var fractalFunction FractalFunction
+
+	if *fractalFunctionArgument == "mandelbrot" {
+		fractalFunction = MandelbrotSet
 	}
+
+	params := ProduceRasterParameters(*renderXArgument, *renderIArgument, *renderWidthArgument,
+		*renderHeightArgument, *renderZoomArgument, *fractalIterationsArgument, 2.0, fractalFunction)
 
 	img := Engine(params)
 
-	f, _ := os.Create("output.png")
+	f, _ := os.Create(*renderOutputArgument)
 	defer f.Close()
 
 	png.Encode(f, img.SubImage(img.Rect))
 }
 
-func ProduceRasterParameters(x float64, i float64, width int, height int, zoom float64, maxIterations int, divergenceThreshold float64) RasterParameters {
+func ProduceRasterParameters(x float64, i float64, width int, height int, zoom float64, maxIterations int, divergenceThreshold float64, fractalFunction FractalFunction) RasterParameters {
 	var scale float64 = 3.2
 
 	delta := scale / float64(width) / zoom
 
 	return RasterParameters{
-		MandelbrotSet,
+		fractalFunction,
 		width,
 		height,
 		i - (float64(height)/2.0)*delta,
